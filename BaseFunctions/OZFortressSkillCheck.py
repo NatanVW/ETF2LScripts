b9f5fe9a82c34aa17d42216b6dc3e477c0bd2d66
NatanVW <natan.vanwelden@gmail.com>
2019-04-05 19:00:15 +0200

    Base for OZFortress skill check

---
+++ BaseFunctions/OZFortressSkillCheck.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+ID = 76561198044051538
+
+def getPlayerPage(ID):
+    url = "https://warzone.ozfortress.com/users?q=" + str(ID)
+    searchPage = requests.get(url).content
+    html = BeautifulSoup(searchPage, "lxml")
+    results = html.find("div", {"class":"user-details"})
+    playerPage = []
+    for a in results.find_all("a", href = True):
+        playerPage.append(a['href'])
+    playerPageUrl = "https://warzone.ozfortress.com" + playerPage[0]
+
+    return playerPageUrl
+
+def getSeasonsPlayed(url):
+    playerPage = requests.get(url).content
+    html = BeautifulSoup(playerPage, "lxml")
+    panelHeadings = []
+    for i in range(0, 4):
+        try:
+            panelHeadings.append(html.find_all('div', {"class": "panel-heading"})[i])
+        except IndexError:
+            break
+    panelLists = []
+    seasonsPlayedHtml = []
+    seasonsPlayed = []
+    if panelHeadings[3].text  == "Leagues":
+        panelLists.append(html.find_all('ul', {"class": "list-group"})[2])
+    if panelLists != []:
+        seasonsPlayedHtml.append(panelLists[0].find_all("li"))
+        for j in range(0, len(seasonsPlayedHtml[0])):
+            seasonsPlayed.append(seasonsPlayedHtml[0][j].text)
+
+    return seasonsPlayed
+
+def higherSkillCheckOZ(seasonPlayed, playerID, higherSkilledPlayerIDListOZ, OZProfile, OZProfileList):
+    if seasonPlayed != []:
+        higherSkilledPlayerIDListOZ.append(playerID)
+        OZProfileList.append(OZProfile)
+
+    return higherSkilledPlayerIDListOZ
