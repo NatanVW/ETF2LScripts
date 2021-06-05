736b21e6ed2603fc98a2f4e25c6c359a46f74f31
NatanVW <natan.vanwelden@gmail.com>
2021-06-05 03:24:15 +0100

    Updated for the new handling of skill contrib

--- ProvTiersAuto/ProvisionalTiersAuto.py
+++ ProvTiersAuto/ProvisionalTiersAuto.py
@@ -7,14 +7,14 @@ from BaseFunctions.ETF2lBase import getCompList, getPlayers, getTeamName
 from ProvTiersAuto.ProvTiersBase import makeTeamDict, getTeamIDList, setGameMode
 
 # Input the team ID list and the requested tier list. Either input a list of strings or a string where each item is seperated by a tab
-idList = "19335	32765	32743	32270	32113	32861	32302	25693	32759	32999	29935	29912	33017	32475	32844	32871	32786	32905	32804	32868	32957	30263	33317	32883	32234	32388	18974	32859	33014	32131	33015	27530	32564	19058	26591	32342	32426	32781	32345	32106	17798	32494	32034	24746	33010	33325	30320	33025	32303	32366	32334	28121	33037	33332	32974	33314	32172	33316	15388	33337	32823	33313	31702	31253	32422	32235	31477	27488	32137	32258	29616	33338	13849	25734	33031	32950	32846	21344	31672	32887	33368	32926	32577	32373	33003	30548	33355	32897"
-requestList = "Premiership	High	Low	Low	High	Low	Mid	Premiership	Open	Open	Open	Premiership	Open	Low	Open	Open	Open	Open	Low	Open	Open	Premiership	Low	High	Mid	Low	Premiership	Open	Open	Open	Open	High	Open	Mid	Mid	High	Low	Low	Open	Mid	High	Open	Low	Premiership	Open	Low	High	High	High	Open	Open	High	High	Open	Low	Open	Premiership	Open	Low	Open	Low	Open	Open	High	Mid	High	Low	Premiership	Low	High	High	Open	Low	Premiership	Low	Low	Open	High	Low	Mid	Open	Mid	Mid	Open	Open	Low	High	High"
+idList = "26973	33328	33680	33687	33656	13849	18974	32334	25693	33644	32388	32781	32957	32366	33631	33314	32786	32871	33527	33603	32475	32137	33559	33654	33634	27530	33523	33031	32897	32212	31684	32303	33673	31253	32362	32172	29616	33696	32342	32905	33337	33610	24746	31678	25734	32883	19335	32106	32804	30263	32972	25568	33316	32234	32500	32396	31477	17798	32422	33727	33695	33729	32373	33717	33611	32868	33690	33526	33734	33742	28121	33037	32861	33692	33355	32743	33721"
+requestList = "Mid	Open	Open	Open	Open	Open	Premiership	Low	Premiership	Low	Low	Low	Open	Low	Open	Open	Mid	Low	Open	Low	Mid	Low	Open	Open	Open	Mid	Open	Mid	High	High	High	High	Open	High	Open	High	Premiership	Mid	Mid	Open	Open	Open	Premiership	High	High	High	Premiership	Mid	Mid	High	Open	High	Open	High	High	Open	Low	Mid	Mid	Premiership	Low	Premiership	Mid	Low	Mid	Low	Mid	Open	Open	Mid	High	High	Mid	Mid	Mid	Low	Low"
 # Set the competition ID and the ID of the competition from which on forward results should be taken into account
-currentMainCompID = 686
-oldCompID = 611
+currentMainCompID = 713
+oldCompID = 628
 
 # Enter the name of the season, will be used as the worksheet title
-seasonName = "HL S23 test"
+seasonName = "HL Season 24"
 
 # Input the gamemode that needs to be checked. HL for highlander, 6s for 6v6
 gameType = "HL"
@@ -67,8 +67,6 @@ def mainSheet(oldCompID, compID, divList, teamIDList, counterDict, teamDict):
         baseSheet = sheet.add_worksheet(title=seasonName + " Base", rows="4", cols="20")
 
     compList6v6, compListHL = getCompList(oldCompID, compID)
-    print(compListHL)
-    print(compList6v6)
     counterList = []
     for value in counterDict.values():
         counterList.append(value)
@@ -77,8 +75,8 @@ def mainSheet(oldCompID, compID, divList, teamIDList, counterDict, teamDict):
     baseSheet.insert_row(row, i)
     i += 1
     for teamID in teamIDList:
-        teamHL = dict(prem=0, div1=0, high=0, mid=0, low=0, open=0, none=0)
-        team6s = dict(prem=0, div1=0, div2=0, mid=0, low=0, open=0, none=0)
+        teamHL = dict(prem=0, div1=0, high=0, div2=0, div3=0, mid=0, div4=0, low=0, div5=0, div6=0, open=0, none=0)
+        team6s = dict(prem=0, div1=0, high=0, div2=0, div3=0, mid=0, div4=0, low=0, div5=0, div6=0, open=0, none=0)
         playerIDList = getPlayers(teamID)
         while playerIDList == []:
             playerIDList = getPlayers(teamID)
@@ -87,12 +85,22 @@ def mainSheet(oldCompID, compID, divList, teamIDList, counterDict, teamDict):
             playerHL, player6s, HLMatchCount, SMatchCount, previousFMC = getPlayerSkill(playerID, compList6v6, compListHL)
             team6s, teamHl = teamSkill(player6s, playerHL, team6s, teamHL, HLMatchCount, SMatchCount)
 
-        Sseperate = 'Prem: ' + str(team6s['prem']) + ', Div1: ' + str(team6s['div1']) + ', Div2: ' + str(team6s['div2']) + ', Mid: ' + str(team6s['mid']) + ', Low: ' + str(
-            team6s['low']) + ', Open: ' + str(team6s['open']) + ', None: ' + str(team6s['none'])
-        STotal = team6s['prem'] * 6 + team6s['div1'] * 5 + team6s['div2'] * 4 + team6s['mid'] * 3 + team6s['low'] * 2 + team6s['open']
-        Hlseperate = 'Prem: ' + str(teamHL['prem']) + ', Div1: ' + str(teamHL['div1']) + ', High: ' + str(teamHL['high']) + ', Mid: ' + str(teamHL['mid']) + ', Low: ' + str(
-            teamHL['low']) + ', Open: ' + str(teamHL['open']) + ', None:' + str(teamHL['none'])
-        HlTotal = teamHL['prem'] * 6 + teamHL['div1'] * 5 + teamHL['high'] * 4 + teamHL['mid'] * 3 + teamHL['low'] * 2 + teamHL['open']
+        Sseperate = 'Prem: ' + str(team6s['prem']) + ', Div1: ' + str(team6s['div1']) + ', high: ' + str(
+            team6s['high']) + ', Div2: ' + str(team6s['div2']) + ', Div3: ' + str(team6s['div3']) + ', Mid: ' + str(
+            team6s['mid']) + ', Div4: ' + str(team6s['div4']) + ', Low: ' + str(
+            team6s['low']) + ', Div5: ' + str(team6s['div5']) + ', Open: ' + str(team6s['div6']) + ', None: ' + str(
+            team6s['none'])
+        STotal = team6s['prem'] * 28 + team6s['div1'] * 24 + team6s['high'] * 22 + team6s['div2'] * 20 + team6s[
+            'div3'] * 16 + team6s['mid'] * 15 + team6s['div4'] * 12 + team6s['low'] * 9 + team6s['div5'] * 8 + team6s[
+                     'div6'] * 4
+        Hlseperate = 'Prem: ' + str(teamHL['prem']) + ', Div1: ' + str(teamHL['div1']) + ', high: ' + str(
+            teamHL['high']) + ', Div2: ' + str(teamHL['div2']) + ', Div3: ' + str(teamHL['div3']) + ', Mid: ' + str(
+            teamHL['mid']) + ', Div4: ' + str(teamHL['div4']) + ', Low: ' + str(
+            teamHL['low']) + ', Div5: ' + str(teamHL['div5']) + ', Open: ' + str(teamHL['div6']) + ', None: ' + str(
+            teamHL['none'])
+        HlTotal = teamHL['prem'] * 28 + teamHL['div1'] * 24 + teamHL['high'] * 22 + teamHL['div2'] * 20 + teamHL[
+            'div3'] * 16 + teamHL['mid'] * 15 + teamHL['div4'] * 12 + teamHL['low'] * 9 + teamHL['div5'] * 8 + teamHL[
+                      'div6'] * 4
         teamLink = "https://etf2l.org/teams/" + str(teamID)
         teamLinkName = '=HYPERLINK("' + teamLink + '";"' + teamName + '")'
 
